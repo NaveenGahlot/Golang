eed84e9ce675227b6160dacb83d87b69fd6f2926
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-07-31 06:02:57 +0530

    Web Request in Golang

---
+++ webreq/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("learning web services")
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	if err != nil {
+		fmt.Println("Error getting GET response", err)
+		return
+	}
+	defer res.Body.Close()
+	fmt.Printf("Type of response:- %T\n", res)
+	// fmt.Println("Response:-", res)
+
+	// Read the response body
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error reading response", err)
+		return
+	}
+	fmt.Println("response:-", string(data))
+}
