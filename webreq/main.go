0e9875260e42f3029ac81e8b6252f21481e224e9
NaveenGahlot <naveengahlotnkt@gmail.com>
2026-07-31 06:19:50 +0530

    Web Request in Golang

--- webreq/main.go
+++ webreq/main.go
@@ -6,13 +6,20 @@ import (
 	"net/http"
 )
 
+// This file demonstrates how to make an HTTP GET request in Go.
+// Definition: This file contains the main function, which is the entry point of the program.
+
 func main() {
 	fmt.Println("learning web services")
+
+	// Send an HTTP GET request to a sample API
 	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	if err != nil {
 		fmt.Println("Error getting GET response", err)
 		return
 	}
+
+	// Close the response body after use
 	defer res.Body.Close()
 	fmt.Printf("Type of response:- %T\n", res)
 	// fmt.Println("Response:-", res)
